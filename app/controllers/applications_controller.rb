c607ac2f6a4ec510b03a4c24d2778e3dc0730ce8
Carina Sweet <carina.sweet06@gmail.com>
2021-07-17 14:46:18 -0600

    User story (#4)

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationsController < ApplicationController
 
+  def index
+  end
+
   def show
     @application = Application.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    application = Application.create(application_params)
+    redirect_to "/applications/#{application.id}"
+  end
+
+private
+  def application_params
+    params.permit(:name, :address, :city, :state, :zip_code, :description, :status)
   end
 end
